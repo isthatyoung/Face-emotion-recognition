a772e2b05774d1bf73c40eca6b17299907660996
Siyang Liu <liusiyang@gwu.edu>
2019-04-08 23:59:13 -0400

    Update mean computation

--- Preprocessing/code/compute_mean.sh
+++ Preprocessing/code/compute_mean.sh
@@ -1,3 +1,4 @@
 DIR=/home/ubuntu/Face-emotion-recognition-master/Preprocessing/
 /home/ubuntu/caffe/build/tools/compute_image_mean $DIR/data/img_train_lmdb/ $DIR/data/train_mean.binaryproto
 /home/ubuntu/caffe/build/tools/compute_image_mean $DIR/data/img_val_lmdb/ $DIR/data/val_mean.binaryproto
+/home/ubuntu/caffe/build/tools/compute_image_mean $DIR/data/img_test_lmdb/ $DIR/data/test_mean.binaryproto
