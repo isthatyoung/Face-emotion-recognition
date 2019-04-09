36e996e2a597925617842f2a5253420eebcef64c
Siyang Liu <liusiyang@gwu.edu>
2019-04-08 22:36:51 -0400

    Update create train/test list script

---
+++ Preprocessing/code/FER2013_get_list_to_LMDB.py
@@ -0,0 +1,35 @@
+import os
+
+
+def main():
+    path = '/home/ubuntu/Face-emotion-recognition-master/Preprocessing/data/'
+    f_train = open(path + 'train_list.txt', 'w')
+    f_validation = open(path + 'validation_list.txt', 'w')
+    f_test = open(path + 'test_list.txt', 'w')
+    train_dir = os.listdir(path + 'fer2013/train')
+    val_dir = os.listdir(path + 'fer2013/val')
+    test_dir = os.listdir(path + 'fer2013/test')
+
+    for dir in train_dir:
+        for image in os.listdir('{}/{}'.format(path + 'fer2013/train', dir)):
+            f_train.writelines('/{}/{} {}'.format(dir,image, dir))
+            f_train.write('\n')
+
+    for dir in val_dir:
+        for image in os.listdir('{}/{}'.format(path + 'fer2013/val', dir)):
+            f_validation.writelines('/{}/{} {}'.format(dir, image, dir))
+            f_validation.write('\n')
+
+    for dir in test_dir:
+        for image in os.listdir('{}/{}'.format(path + 'fer2013/test', dir)):
+            f_test.writelines('/{}/{} {}'.format(dir,image, dir))
+            f_test.write('\n')
+
+    f_train.close()
+    f_validation.close()
+    f_test.close()
+
+
+
+if __name__ == '__main__':
+    main()
