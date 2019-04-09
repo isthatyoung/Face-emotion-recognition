3b512a1855414c4660b876a423e02409e89d4f4d
Siyang Liu <liusiyang@gwu.edu>
2019-04-08 22:48:03 -0400

    Update train.py

--- Train/code/train.py
+++ Train/code/train.py
@@ -7,7 +7,7 @@ caffe.set_mode_gpu()
 dir = '/home/ubuntu/Face-emotion-recognition-master/Train/code/'
 
 model = dir + 'FaceEmotionNet_model.prototxt'
-solver_proto = dir + 'FaceEmotionNet_model_solver.prototxt'
+solver_proto = dir + 'FaceEmotionNet_solver.prototxt'
 
 solver = caffe.SGDSolver(solver_proto)
 solver.solve()
