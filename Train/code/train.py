22396f11a8acb7f9dfaf4d6498d12012ecf35318
Siyang Liu <liusiyang@gwu.edu>
2019-04-08 22:39:27 -0400

    Update Train

--- Train/code/train.py
+++ Train/code/train.py
@@ -4,10 +4,10 @@ from caffe.proto import caffe_pb2
 caffe.set_device(0)
 caffe.set_mode_gpu()
 
-dir = '/home/ubuntu/face-emotion-detection/train/'
+dir = '/home/ubuntu/Face-emotion-recognition-master/Train/code/'
 
-model = dir + 'VGG_ILSVRC_16_layers_model.prototxt'
-solver_proto = dir + 'VGG_ILSVRC_16_solver.prototxt'
+model = dir + 'FaceEmotionNet_model.prototxt'
+solver_proto = dir + 'FaceEmotionNet_model_solver.prototxt'
 
 solver = caffe.SGDSolver(solver_proto)
 solver.solve()
