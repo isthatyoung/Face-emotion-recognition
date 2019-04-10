ae04f724ce8b88b89b63fca04a778dbbb29099b0
Siyang Liu <liusiyang@gwu.edu>
2019-04-09 23:43:34 -0400

    Training using pycaffe

--- Train/code/train.py
+++ Train/code/train.py
@@ -1,13 +1,73 @@
 import caffe
 from caffe.proto import caffe_pb2
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (8, 8)
 
-caffe.set_device(0)
-caffe.set_mode_gpu()
+def main():
+    caffe.set_device(0)
+    caffe.set_mode_gpu()
+    dir = '/home/ubuntu/Face-emotion-recognition-master/Train/code/'
+    solver = load_solver(dir)
+    loss, accuracy, test_interval = train(solver)
+    plot_error(loss)
+    plt.figure()
+    plot_accuracy(accuracy, test_interval)
+    plt.show()
 
-dir = '/home/ubuntu/Face-emotion-recognition-master/Train/code/'
+def load_solver(dir):
+    solver_proto = dir + 'FaceEmotionNet_solver.prototxt'
+    solver = caffe.SGDSolver(solver_proto)
+    return solver
+def train(solver):
+    niter = 60000
+    test_interval = 1000
+    train_loss = np.zeros(niter)
+    test_acc = np.zeros(int(np.ceil(niter / test_interval)))
+    display = 100
+    for i in range(niter):
+        solver.step(1)
+        train_loss[i] = solver.net.blobs['Softmax_loss'].data
+        if i % display == 0:
+            print('Iteartion {}, Train loss: {}'.format(i,train_loss[i]))
+        solver.test_nets[0].forward(start='conv1')
 
-model = dir + 'FaceEmotionNet_model.prototxt'
-solver_proto = dir + 'FaceEmotionNet_solver.prototxt'
+        if i % test_interval == 0:
+            acc = solver.test_nets[0].blobs['accuracy'].data
+            print('Iteration', i, 'testing...', 'accuracy:', acc)
+            test_acc[i // test_interval] = acc
 
-solver = caffe.SGDSolver(solver_proto)
-solver.solve()
+    solver.net.save('/home/ubuntu/Face-emotion-recognition-master/Train/data/FaceEmotionNet.caffemodel')
+    return train_loss, test_acc, test_interval
+
+def plot_error(loss):
+    plt.subplot(1, 2, 1)
+    iteration = np.arange(1, len(loss) + 1)
+    plt.plot(iteration, loss, c='blue', alpha = 0.3)
+    plt.xlabel('Number of Iteration')
+    plt.ylabel('Training Loss')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.title('Train loss vs iteration')
+
+
+
+
+def plot_accuracy(accuracy, test_interval):
+    plt.subplot(1,2,1)
+    plt.plot(test_interval*np.arange(len(accuracy)), accuracy, c='#56fca2')
+    plt.xlabel('Number of Iteration')
+    plt.ylabel('Accuracy')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.title('Test accuracy vs iteration on FAR2013')
+    plt.savefig('/home/ubuntu/accuracy.png')
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
