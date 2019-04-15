09b47b733cf6288f0396b512ac750f299c87ca5d
Siyang Liu <liusiyang@gwu.edu>
2019-04-15 19:20:51 -0400

    Add CK dataset testing by fine-tuning

--- Preprocessing/code/CK48_get_train_test_list.py
+++ Preprocessing/code/CK48_get_train_test_list.py
@@ -1,19 +1,19 @@
 import os
 def main():
     
-    path = '/home/ubuntu/face-emotion-detection/data/'
-    f_train = open(path+'train_list.txt', 'w+')
-    f_test = open(path+'test_list.txt', 'w+')
+    path = '/home/ubuntu/Face-emotion-recognition-master/Preprocessing/data/'
+    f_train = open(path+'CK48_train_list.txt', 'w+')
+    f_test = open(path+'CK48_test_list.txt', 'w+')
     dirs = os.listdir(path+'CK+48')
     for dir in dirs:
         count = 0
         for image in os.listdir('{}/{}'.format(path+'CK+48', dir)):
             num_image = len(os.listdir('{}/{}'.format(path+'CK+48', dir)))
             if count >= 0.8*num_image:
-                f_test.writelines('{}CK+48/{}/{} {}'.format(path,dir,image,dir))
+                f_test.writelines('/{}/{} {}'.format(dir, image, dir))
                 f_test.write('\n')
             else:
-                f_train.writelines('{}CK+48/{}/{} {}'.format(path,dir,image,dir))
+                f_train.writelines('/{}/{} {}'.format(dir, image, dir))
                 f_train.write('\n')
                 count += 1
     f_train.close()
--- Preprocessing/code/convert_lmdb_CK48.sh
+++ Preprocessing/code/convert_lmdb_CK48.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 MY=/home/ubuntu/Face-emotion-recognition-master/Preprocessing/data
 
+echo "Create CK48 train lmdb.."
+rm -rf $MY/CK48_test_lmdb
+/home/ubuntu/caffe/build/tools/convert_imageset \
+--shuffle \
+--resize_height=48 \
+--resize_width=48 \
+--gray \
+/home/ubuntu/Face-emotion-recognition-master/Preprocessing/data/CK+48 \
+$MY/CK48_train_list.txt \
+$MY/CK48_train_lmdb
+
 echo "Create CK48 test lmdb.."
 rm -rf $MY/CK48_test_lmdb
 /home/ubuntu/caffe/build/tools/convert_imageset \
@@ -9,5 +20,5 @@ rm -rf $MY/CK48_test_lmdb
 --resize_width=48 \
 --gray \
 /home/ubuntu/Face-emotion-recognition-master/Preprocessing/data/CK+48 \
-$MY/CK_48_test.txt \
+$MY/CK48_test_list.txt \
 $MY/CK48_test_lmdb
